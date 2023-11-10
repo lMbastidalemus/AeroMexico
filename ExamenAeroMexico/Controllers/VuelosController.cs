bee3ccaea15b2d2652b1969863589a0ca1a8ea67
lMbastidalemus <lbastidalemus@gmail.com>
2023-11-10 12:01:39 -0600

    Vuelos

--- ExamenAeroMexico/Controllers/VuelosController.cs
+++ ExamenAeroMexico/Controllers/VuelosController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -18,10 +20,34 @@ namespace ExamenAeroMexico.Controllers
         [HttpPost]
         public ActionResult VuelosV(ML.Vuelos vuelos)
         {
-            
-            ML.Result resultVuelos = BL.Vuelos.GetAll(vuelos.FechaInicio, vuelos.FechaSalida);
-            vuelos = new ML.Vuelos();
-            vuelos.Vueloss = resultVuelos.Objects;
+
+            using (var client = new HttpClient())
+            {
+                ML.Result resultVuelos = BL.Vuelos.GetAll(vuelos.FechaInicio, vuelos.FechaSalida);
+                vuelos = new ML.Vuelos();
+                vuelos.Vueloss = resultVuelos.Objects;
+
+
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["WebApi"]);
+
+                var responseTask = client.GetAsync("");
+                responseTask.Wait();
+
+                var resultServicio = responseTask.Result;
+
+                if (resultServicio.IsSuccessStatusCode)
+                {
+                    var readTask = resultServicio.Content.ReadAsAsync<ML.Result>();
+                    readTask.Wait();
+
+                    foreach (var resultItem in readTask.Result.Objects)
+                    {
+                        ML.Vuelos resultItemList = Newtonsoft.Json.JsonConvert.DeserializeObject<ML.Vuelos>(resultItem.ToString());
+                        vuelos.Vueloss.Add(resultItemList);
+                    }
+                }
+            }
+
             return View(vuelos);
 
         }
@@ -41,17 +67,41 @@ namespace ExamenAeroMexico.Controllers
         [HttpPost]
         public ActionResult Reservacion(string NumeroVuelo, string Nombre)
         {
-            ML.Result result = BL.Reservacion.Add(NumeroVuelo, Nombre);
-            if (result.Correct)
-            {
-                ViewBag.Mensaje = "Add";
-            }
-            else
-            {
-                ViewBag.Mensaje = "Error";
-            }
-            return PartialView("Modal");
+            //ML.Result result = BL.Reservacion.Add(NumeroVuelo, Nombre);
+            //if (result.Correct)
+            //{
+            //    ViewBag.Mensaje = "Add";
+            //}
+            //else
+            //{
+            //    ViewBag.Mensaje = "Error";
+            //}
+            //return PartialView("Modal");
+
+            //using (var client = new HttpClient())
+            //{
+
+
+            //    client.BaseAddress = new Uri(ConfigurationManager.AppSettings["WebApi"]);
+            //    var postTask = client.PostAsJsonAsync<ML.Vuelos>("", NumeroVuelo, Nombre);
+
+            //    postTask.Wait();
+            //    var result = postTask.Result;
+            //    if (result.IsSuccessStatusCode)
+            //    {
+            //        ViewBag.Mensaje = "Agregado correctamente";
+            //        return View("Modal");
+            //    }
+            //    else
+            //    {
+            //        ViewBag.Mensaje = "Error al agregar";
+            //        return View("Modal");
+            //    }
+            //}
+            return View();
         }
+
+
 
        
     }
---
+++ SL/Controllers/VuelosController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SL.Controllers
+{
+    [RoutePrefix("api/vuelos")]
+    public class VuelosController : ApiController
+    {
+        [Route("")]
+        [HttpPost]
+        public IHttpActionResult GetAll(ML.Vuelos vuelos)
+        {
+            ML.Result result = BL.Vuelos.GetAll(vuelos.FechaInicio, vuelos.FechaSalida);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+
+        [Route("Add")]
+        [HttpPost]
+        public IHttpActionResult Reservacion(string NumeroVuelo, string Nombre)
+        {
+            ML.Result result = BL.Reservacion.Add(NumeroVuelo, Nombre);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+    }
+}
